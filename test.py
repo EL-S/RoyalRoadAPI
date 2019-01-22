0502aebac6884e607b82db70011a081115189842
EL-S <31227083+EL-S@users.noreply.github.com>
2019-01-22 23:17:28 +1100

    Removed Comments from Example
    Removed the comments to allow the example to fully display functionality

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from royalroadlapi import *
 
-#fiction = get_fiction_object(17364)
-#data = get_fiction_info(17363)
+fiction = get_fiction_object(17364)
+data = get_fiction_info(17363)
 
-#get_fictions("11343","11343",directory="Fiction - Epubs/")
+get_fictions("11343","11343",directory="Fiction - Epubs/")
 
 print(get_fiction_location("8894",directory="Fiction - Epubs/"))
 print(get_fiction_location("8894",directory="Fiction - Epubs/",start_chapter="1",end_chapter="5"))
@@ -13,10 +13,10 @@ get_fiction("8894",directory="Fiction - Epubs/",start_chapter="1",end_chapter="5
 get_fiction("8894",directory="Fiction - Epubs/",start_chapter="5",end_chapter="5")
 get_fiction("8894",directory="Fiction - Epubs/",start_chapter="307",end_chapter="307")
 get_fiction("8894",directory="Fiction - Epubs/",start_chapter="1",end_chapter="1")
-##get_fiction("8894",directory="Fiction - Epubs/",start_chapter="2",end_chapter="5")
-##get_fiction("8894",directory="Fiction - Epubs/",start_chapter="3",end_chapter="900")
-##get_fiction("8894",directory="Fiction - Epubs/",start_chapter="3")
-##get_fiction("8894",directory="Fiction - Epubs/",end_chapter="3")
-##get_fiction("8894",directory="Fiction - Epubs/",start_chapter="800",end_chapter="900")
-##get_fiction("8894",directory="Fiction - Epubs/",start_chapter="-20",end_chapter="900")
-##
+get_fiction("8894",directory="Fiction - Epubs/",start_chapter="2",end_chapter="5")
+get_fiction("8894",directory="Fiction - Epubs/",start_chapter="3",end_chapter="900")
+get_fiction("8894",directory="Fiction - Epubs/",start_chapter="3")
+get_fiction("8894",directory="Fiction - Epubs/",end_chapter="3")
+get_fiction("8894",directory="Fiction - Epubs/",start_chapter="800",end_chapter="900")
+get_fiction("8894",directory="Fiction - Epubs/",start_chapter="-20",end_chapter="900")
+
