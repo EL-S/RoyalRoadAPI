a9d86be7c86d413a9319a894f3c7a10864a544a0
EL-S <31227083+EL-S@users.noreply.github.com>
2019-02-02 23:58:28 +1100

    Read and delete all types of messages
    Ability to now read and delete every type of message has been implemented

--- login.py
+++ login.py
@@ -165,27 +165,33 @@ def send_message(login_object,recipients,subject,message,replyto=""):
         
         return status
 
-
-def read_messages(login_object):
-    if login_object == None:
-        print("Failed to Read Messages: Not Logged In.")
-        return
+def request_message_like_list(login_object,message_type):
+    url = "https://www.royalroad.com/private/"
+    if message_type == "received":
+        url_suffix = "1"
+    elif message_type == "sent":
+        url_suffix = "2"
+    elif message_type == "drafts":
+        url_suffix = "3"
+    elif message_type == "deleted":
+        url_suffix = "4"
     else:
-        url = "https://www.royalroad.com/private"
-        soup = request_secure_page(url,login_object)
-        try:
-            pages = int(soup.find("ul", attrs={"class":"pagination"}).findAll("a")[-1].get("href").split("=")[-1])
-        except:
-            pages = 1
-        messages = []
-        messages = extract_messages_from_soup(soup,messages)
-        if pages > 1:
-            for i in range(2,pages+1):
-                url_page = str(url)+"?page="+str(i)
-                print(url_page)
-                soup = request_secure_page(url_page,login_object)
-                messages = extract_messages_from_soup(soup,messages)
-        return messages
+        url_suffix = "1"
+    url = url+url_suffix
+    soup = request_secure_page(url,login_object)
+    try:
+        pages = int(soup.find("ul", attrs={"class":"pagination"}).findAll("a")[-1].get("href").split("=")[-1])
+    except:
+        pages = 1
+    messages = []
+    messages = extract_messages_from_soup(soup,messages)
+    if pages > 1:
+        for i in range(2,pages+1):
+            url_page = str(url)+"?page="+str(i)
+            print(url_page)
+            soup = request_secure_page(url_page,login_object)
+            messages = extract_messages_from_soup(soup,messages)
+    return messages
 
 def extract_messages_from_soup(soup,messages):
     message_listings = soup.find("tbody").findAll("tr", recursive=False)
@@ -196,18 +202,138 @@ def extract_messages_from_soup(soup,messages):
         time_data = data[3].find("time")
 
         message_id = data2.find("a").get("href").split("/")[-1].strip()
-        author = data3.text.strip()
         author_id = data3.find("a").get("href").split("/")[-1].strip()
+        author = data3.text.strip()
         title = data2.text.strip()
         status = data[0].find("i").get("title").strip()
         time = [time_data.get("unixtime").strip(),time_data.text.strip()]
-        messages.append([message_id,author,author_id,title,status,time])
+        messages.append([message_id,author_id,author,title,status,time])
     return messages
+
+def read_messages(login_object):
+    if login_object == None:
+        print("Failed to Read Messages: Not Logged In.")
+        return
+    else:
+        message_type = "received"
+        received_messages = request_message_like_list(login_object,message_type)
+        return received_messages
+
+def read_sent_messages(login_object):
+    if login_object == None:
+        print("Failed to Read Sent Messages: Not Logged In.")
+        return
+    else:
+        message_type = "sent"
+        sent_messages = request_message_like_list(login_object,message_type)
+        return sent_messages
+
+def read_draft_messages(login_object):
+    if login_object == None:
+        print("Failed to Read Draft Messages: Not Logged In.")
+        return
+    else:
+        message_type = "drafts"
+        draft_messages = request_message_like_list(login_object,message_type)
+        return draft_messages
+
+def read_deleted_messages(login_object):
+    if login_object == None:
+        print("Failed to Read Deleted Messages: Not Logged In.")
+        return
+    else:
+        message_type = "deleted"
+        deleted_messages = request_message_like_list(login_object,message_type)
+        return deleted_messages
+
+def get_message_content(login_object,message_id):
+    if login_object == None:
+        print("Failed to Read Message: Not Logged In.")
+        return
+    else:
+        url = "https://www.royalroad.com/private/read/"+message_id
+        soup = request_secure_page(url,login_object)
+        content_data = extract_message_content(soup)
+        if content_data != None:
+            content = [message_id,content_data[0],content_data[1],content_data[2],content_data[3],content_data[4],content_data[5],content_data[6]]
+            return content
+
+def extract_message_content(soup):
+    try:
+        data = soup.find("div", attrs={"class":"pm-users"}).findAll("a")
+        time_data = soup.find("time")
+        title = soup.find("h3", attrs={"class":"margin-top-0 margin-bottom-10"}).text.strip()
+        content = soup.find("div", attrs={"class":"pm-body"}).text.strip()
+        author = data[0].text.strip()
+        author_id = data[0].get("href").split("/")[-1].strip()
+        recipient = data[1].text.strip()
+        recipient_id = data[1].get("href").split("/")[-1].strip()
+        time = [time_data.get("unixtime"),time_data.text.strip()]
+        return [author_id,recipient_id,author,recipient,title,content,time]
+    except:
+        print("That message has been deleted, it does not exist, or you do not have permission to view it.")
+
+def do_secure_post(login_object,token_url,post_url,message_id): #only deletes at the moment
+    cookie = login_object[0]['cookie'][:-1]
+    soup = request_secure_page(token_url, login_object)
+
+    requesttoken = soup.find("input", attrs={"name":"__RequestVerificationToken"}).get("value")
+    
+    cfuid = cookie.split(";")[0].split("=")[1]
+    ga = cookie.split(";")[1].split("=")[1]
+    visited = cookie.split(";")[3].split("=")[1]
+    notif_dismiss = "1"
+    royalroad_sessionid = cookie.split(";")[4].split("=")[1]
+    gid = cookie.split(";")[2].split("=")[1]
+    
+    antiforgery = cookie.split(";")[5].split("=")[1]
+    identity_application = cookie.split(";")[6].split("=")[1]
+    
+    cookie_new = "__cfduid="+cfuid+"; _ga="+ga+"; visited="+visited+"; notif_dismiss-987973="+notif_dismiss+"; RoyalRoad.SessionId="+royalroad_sessionid+"; .AspNetCore.Antiforgery.w5W7x28NAIs="+antiforgery+"; _gid="+gid+"; .AspNetCore.Identity.Application="+identity_application
+
+    action = "delete" #maybe
+
+    data = {"__RequestVerificationToken": requesttoken,
+            "pmid": message_id}
+    url_encoded = urllib.parse.urlencode(data) #maybe this is wrong
+    content_length = str(len(url_encoded))
+    headers = {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                "accept-encoding": "gzip, deflate",
+                "accept-language": "en-US,en;q=0.9",
+                "cache-control": "max-age=0",
+                "content-length": content_length, #can be anything
+                "content-type": "application/x-www-form-urlencoded",
+                "cookie": cookie_new, #must be correct
+                "origin": "https://www.royalroad.com",
+                "referer": token_url, #can be anything?
+                "upgrade-insecure-requests": "1",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
+
+    req = requests.post(post_url, data=data, headers=headers)
+
+    soup = BeautifulSoup(req.text, "lxml")
+    title = soup.find("title").text
+    return title
+
+def delete_message(login_object,message_id):
+    if login_object == None:
+        print("Failed to Delete Message: Not Logged In.")
+        status = False
+        return status
+    else:
+        token_url = "https://www.royalroad.com/private/read/"+message_id
+        post_url = "https://www.royalroad.com/private/delete"
+        title = do_secure_post(login_object,token_url,post_url,message_id)
+        if "pm deleted".lower() in title.lower():
+            status = True
+            print("Message deleted.")
+        else:
+            status = False
+            print("Message failed to delete.")
+        return status
 
 login_object = login("username","password")
 
-messages = read_messages(login_object)
+#status = delete_message(login_object,"message_id")
 
-print(messages)
-
-#status = send_message(login_object,"userid","subject","message")
+#status = send_message(login_object,"user_id","subject","message")
