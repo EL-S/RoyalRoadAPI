4b084ea99d983ebbe9108c12ab5f040b36dbd1ff
EL-S <31227083+EL-S@users.noreply.github.com>
2019-06-20 22:48:37 +1000

    Run Time Plans
    Plan to improve the programs runtime and use local files to improve speed

--- royalroadlapi.py
+++ royalroadlapi.py
@@ -22,6 +22,7 @@ file_name_chapter_range = "" #to have a default empty chapter range expression
 # move epub and storage code to separate files
 # autodetect if file is updated
 # improve threading by downloading all chapters from all fictions at once, sorting into folders as it is received
+# sort main file out to improve run time
 def get_fiction(fiction_id,directory="Fictions/",start_chapter="first",end_chapter="last"): #download a fiction by id or search
     global epub_index_start,file_name_chapter_range,final_location,plural #access global variables
     try:
